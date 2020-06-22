4a94599aa149f4b58ba0a7e91ee8428234602fa5
wassef911 <1.wassef911@gmail.com>
2020-06-22 14:56:36 +0100

    added gzip compression

--- script2.js
+++ script2.js
@@ -1,22 +1,29 @@
 #!/usr/bin/env node
-// var getStdin = require("get-stdin");
-var fs = require("fs");
-var path = require("path");
 
-var args = require("minimist")(process.argv.slice(2), {
-  boolean: ["help", "in"],
+const util = require("util");
+const fs = require("fs");
+const path = require("path");
+const Transform = require("stream").Transform;
+const zlib = require("zlib");
+
+const args = require("minimist")(process.argv.slice(2), {
+  boolean: ["help", "in", "out", "compress"],
   string: ["file"],
 });
 
 const BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname);
+let OUT_FILE = path.join(BASE_PATH, "out.txt");
 
 const printHelp = () => {
   /* i'll do my best to keep this updated, no promises tho' */
-  console.log("script1 usage : ./script2 --arg ");
+  console.log("script2 usage : ./script2 --arg ");
   console.log("");
-  console.log("--help               print this help");
-  console.log("--file={FILENAME}    process the file");
-  console.log("cat {FILENAME} | ./script1.js --in  process the file");
+  console.log("--help               print this help.");
+  console.log("--file={FILENAME}    process the file.");
+  console.log("--out                pipe output to a new file.");
+  console.log("--compress           gzip the outputed file.");
+  console.log("");
+  console.log("cat {FILENAME} | ./script2.js --in  process the piped stream.");
   console.log("");
 };
 
@@ -30,7 +37,24 @@ const error = (msg, includeHelp = false) => {
 
 const processFile = (inStream) => {
   let outStream = inStream;
-  let targetStream = process.stdout;
+  let upperStream = new Transform({
+    transform(chunk, enc, next) {
+      this.push(chunk.toString().toUpperCase());
+      setTimeout(next, 400);
+    },
+  });
+  outStream = outStream.pipe(upperStream);
+  let targetStream;
+  if (args.compress) {
+    let gzipStream = zlib.createGzip();
+    outStream = outStream.pipe(gzipStream);
+    OUT_FILE = `${OUT_FILE}.gz`;
+  }
+  if (args.out) {
+    targetStream = process.stdout;
+  } else {
+    targetStream = fs.createWriteStream(OUT_FILE);
+  }
   outStream.pipe(targetStream);
 };
 
